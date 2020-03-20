0e8042b291270787fa81f625fb4bc70d00cdb843
Deniz <sea-97@hotmail.com>
2020-03-20 23:33:48 +0300

    fonksiyonlar template yapısına göre düzenlendi + oto korelasyon eklendi

--- crosscorr.cpp
+++ crosscorr.cpp
@@ -6,34 +6,13 @@ using namespace std;
 const double Enumber = std::exp(1.0);
 const double PI  =3.141592653589793238463;
 
-/*https://www.statisticshowto.datasciencecentral.com/cross-correlation/*/
-/*https://www.drdobbs.com/cpp/a-simple-and-efficient-fft-implementatio/199500857?pgno=1  */
-
-
-float fmean(float arr1[] ,int size);
-float fvariance(float arr1[] ,int size);
-float fcovariance(float arr1[] ,float arr2[],int size,int size1);
-float fcorrelation (float arr1[] ,float arr2[],int size,int size1);
 
 
 
-
-int main() 
-{ 
-   float  a[] = {1, 2, 3, 4, 5, 6,11}; 
-    float  b[] = {1, 2, 3, 4, 5, 6,12}; 
-    int len = sizeof(a)/sizeof(a[0]);
-     int len1 = sizeof(b)/sizeof(b[0]);
-    fcorrelation(a,b,len,len1);
-    
-    return 0; 
-}
-
-
-/* ortalama fonksiyonu*/
-float fmean(float arr1[] ,int size){
-float sum=0;
-float mean;
+template <class T,class Y>
+T fmean(T arr1[] ,Y size){
+T sum=0;
+T mean;
    for(int i=0; i<size; i++){
     sum +=  arr1[i];
    }
@@ -41,25 +20,29 @@ float mean;
    return mean;
 }
 
-float fvariance(float arr1[] ,int size){
-     float mean;float sum=0;float variance;
-     mean = fmean(arr1,size);
+
+template <class T,class Y>
+T fvariance(T arr1[] ,Y size){
+     T sum=0;T variance; 
+     T mean =fmean(arr1,size);
       for(int i=0; i<size; i++){
       sum +=  pow(arr1[i]-mean,2);
    }
   variance =sum/size;
   return variance;
+  
 
 }
 
 
-/* kovaryans fonksiyonu*/
-float fcovariance(float arr1[] ,float arr2[],int size,int size1){
-float sum;
-float mean=fmean(arr1,size);
-float mean1=fmean(arr2,size1);
-float covariance ;
 
+
+template <class T,class Y>
+T fcovariance(T arr1[] ,T arr2[],Y size,Y size1){
+T sum=0;
+T covariance ;
+T mean =fmean(arr1,size);
+T mean1 =fmean(arr2,size1);
 if ( size != size1)
 {
     cout << "verisetlerindeki eleman sayısı eşit değil";
@@ -69,29 +52,74 @@ if ( size != size1)
    sum +=  ((arr1[i]-mean)*(arr2[i]-mean1));
    }
  covariance=sum /size;
-   
-
    }
+return covariance;
 }
 
-/* korelasyon fonksiyonu*/
-float fcorrelation (float arr1[] ,float arr2[],int size,int size1){
-float corelation;
-float first;
-float second;
 
+
+
+template <class T,class Y>
+T fcorrelation (T arr1[] ,T arr2[],Y size,Y size1){
+T corelation;
+T first;
+T second;
 first =fcovariance( arr1 , arr2, size, size1);
 second= sqrt(fvariance(arr1,size)*fvariance(arr2,size1));
 corelation=first/second;
-cout << "korelasyon :"<< corelation;
+cout << "korelasyon :\n"<< corelation;
 return corelation;
 }
 
 
-/* ayrık fourier dönüşüm fonksiyonu bitmedi daha */
+/*https://www.alanzucconi.com/2016/06/06/autocorrelation-function/ */
+template <class T>
+T lagoperator(T size,T delaytime){
+size=size-delaytime;
+}
+
+
+template <class T,class Y>
+T fautocorrelation (T arr1[] ,T arr2[],Y size,Y size1,Y delaytime){
+  T corelation;
+  T first;
+  T second;
+  first =fcovariance( arr1 , arr2, lagoperator(size,delaytime), lagoperator(size1,delaytime));
+  second= sqrt(fvariance(arr1,lagoperator(size,delaytime))*fvariance(arr2,lagoperator(size1,delaytime)));
+  corelation=first/second;
+  cout << "korelasyon :\n"<< corelation;
+  return corelation;
+}
+
+int main() 
+{ 
+   /* örnek verisetleri */
+   float  a[] = {1, 2, 3, 4, 5, 6,11}; 
+   float  b[] = {1, 2, 3, 4, 5, 6,12}; 
+
+   /* örnek verisetlerinin uzunlukları */
+   int len = sizeof(a)/sizeof(a[0]);
+   int len1 = sizeof(a)/sizeof(a[0]);
+
+/*
+Eğer bir örneklem üzerinde çalışılıyorsa n-1 yani uzunlukları n-1 şeklinde 
+yani bu kodu 
+   int len = (sizeof(a)/sizeof(a[0]))-1;https://www.youtube.com/watch?v=htCj9exbGo0&t=52s
+
+*/
+fautocorrelation(a,b,len,len1,2);
+ 
+   
+    return 0; 
+}
+
+
+
+/* ayrık fourier dönüşüm fonksiyonu bitmedi daha 
 float discfourier(float arr1[],int size){
     const   complex<double> i(0.0,1.0);
 double g =pow(Enumber,-2*PI*size);
 
 
 }
+*/
